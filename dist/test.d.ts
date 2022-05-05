0deba0cba0d7197d08f2c5c4f4db21a1ed0317e9
Weng,David D <d.weng@student.fontys.nl>
2022-05-05 08:10:47 +0200

    initial commit

---
+++ dist/test.d.ts
@@ -0,0 +1,3 @@
+export declare class Test {
+    sayHelloTo(name: string): string;
+}
---
+++ dist/test.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Test = void 0;
+var Test = /** @class */ (function () {
+    function Test() {
+    }
+    Test.prototype.sayHelloTo = function (name) {
+        return " Hello World ".concat(name, " !");
+    };
+    return Test;
+}());
+exports.Test = Test;
+//# sourceMappingURL=test.js.map
